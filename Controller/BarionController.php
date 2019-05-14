bcb03f8912b737138e13fecd61279a1c2f156689
Roli <roland.bodnar.80@gmail.com>
2019-05-14 16:54:37 +0200

    - barion wrapper fixes

--- Controller/BarionController.php
+++ Controller/BarionController.php
@@ -7,8 +7,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Vaszev\BarionBundle\Service\Barion;
 
 class BarionController extends AbstractController {
 
@@ -19,28 +17,10 @@ class BarionController extends AbstractController {
       return $locale;
     }
 
-
     return $default;
   }
 
 
-  public function test(Barion $barion) {
-    $redirectURL = $this->generateUrl('index',[],UrlGeneratorInterface::ABSOLUTE_URL);
-    try {
-      $barion->init('http://asdasd.asd', '')
-             ->createTransactionModel('komment');
-
-      for ($i = 1; $i <= 5; $i++) {
-        $barion->addItem('terméknév' . $i,'leírás', $i, 100 * $i, "skuNumber" . $i);
-      }
-      $barion->preparePaymentRequestModel('user@example@com', '1234 Cím, utca hsz.')
-             ->send();
-
-    } catch (\Exception $e) {
-      dump($e);
-    }
-
-  }
 
   /**
    * @param Request $request
